9532166ced479159d83e9cfdcb46184a135539d1
Rakshitha-parashivamurthy <rakshithap107@gmail.com>
2026-03-20 14:13:51 +0530

    first commit

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,31 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD82Pxf7qI-pQ6rRQP56AXLkaJqG_j4Eh0",
-  authDomain: "todo-app-project-aa19e.firebaseapp.com",
-  projectId: "todo-app-project-aa19e",
-  storageBucket: "todo-app-project-aa19e.firebasestorage.app",
-  messagingSenderId: "261014157661",
-  appId: "1:261014157661:web:e3c3fd33df343e79f250b4",
+  apiKey: "AIzaSyD8dSAOl1AJmKcMh5AHlRlp6OjZ_61NmMk",
+  authDomain: "taskmaster-31e08.firebaseapp.com",
+  projectId: "taskmaster-31e08",
+  storageBucket: "taskmaster-31e08.firebasestorage.app",
+  messagingSenderId: "360381471174",
+  appId: "1:360381471174:web:cb0551794c3f4e5dea7ac7",
+  measurementId: "G-F11E5V8LB4"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// ✅ Export these
+// ✅ ADD THESE
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+
+const analytics = getAnalytics(app);
+
+export default app;
